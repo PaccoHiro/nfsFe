b2440e6ef7d94402703611feac6e3edc1eacbd94
PaccoHiro <m.bejdigane@gmail.com>
2022-12-23 14:24:36 +0100

    widgets

--- nfsFe/src/pages/router.jsx
+++ nfsFe/src/pages/router.jsx
@@ -5,12 +5,13 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import Layout from "../layout/Layout";
+import Dashboard from "./dashboard/Dashboard";
 
 const Root = createRoutesFromElements(
   <>
     <Route element={<Layout />}>
       <Route path="/">
-        <Route index element={<App />} />
+        <Route index element={<Dashboard />} />
       </Route>
     </Route>
   </>
