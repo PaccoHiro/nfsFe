c1133634b8baa8e428b0b190e938d2780da5ddb6
PaccoHiro <m.bejdigane@gmail.com>
2022-12-23 20:29:18 +0100

    widgets static 90%

--- nfsFe/src/components/widget/Widget.jsx
+++ nfsFe/src/components/widget/Widget.jsx
@@ -1,10 +1,23 @@
 import "./widget.scss";
+import ArrowDropUpOutlinedIcon from "@mui/icons-material/ArrowDropUpOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Widget = () => {
   return (
     <div className="widget">
-      <div className="left">left</div>
-      <div className="right">right</div>
+      <div className="left">
+        <div className="title">Orders</div>
+        <div className="counter">100</div>
+        <div className="link">see all Orders</div>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <ArrowDropUpOutlinedIcon className="pericon" />
+          <span>20%</span>
+        </div>
+
+        <ShoppingCartOutlinedIcon className="icon" />
+      </div>
     </div>
   );
 };
