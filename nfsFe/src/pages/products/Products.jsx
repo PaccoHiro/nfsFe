bbd091396ea0f8520e8e9eb02ab501a0cf1b8cbb
PaccoHiro <m.bejdigane@gmail.com>
2022-12-26 23:22:06 +0100

    start products list page

---
+++ nfsFe/src/pages/products/Products.jsx
@@ -0,0 +1,11 @@
+import Datatable from "../../components/datatable/Datatable";
+import "./products.scss";
+
+const Products = () => {
+  return (
+    <div className="products">
+      <Datatable />
+    </div>
+  );
+};
+export default Products;
